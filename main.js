a43af6a9b82aa6c2af99a0e5d7fd7171ad4f08f9
anthocodeur <antho.codeur@gmail.com>
2024-08-01 08:50:51 +0200

    fix bugs

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import "./style.scss";
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menu.classList.contains("active")
+    ? (document.body.style.overflow = "hidden")
+    : (document.body.style.overflow = "visible");
+});
